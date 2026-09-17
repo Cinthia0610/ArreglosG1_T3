7c2512e2c998ad5e78f92abdff39688488426a41
Cinthia0610 <25030718@itesa.edu.mx>
2026-09-17 14:49:34 -0600

    Versión_02: Metodo llenar.

---
+++ Arreglos.Logica/MiArreglo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Arreglos.Logica
+{
+    public class MiArreglo
+    {
+        //Atributos o campos
+        private int _tope;
+        private int[] _arreglo;
+
+        //Constructor
+        public MiArreglo(int n)
+        {
+            N = n;
+            _arreglo = new int[N];
+            _tope = 0;
+        }
+        //Propiedades
+        public int N { get; }
+        public bool EstaLleno => _tope == N;
+        public bool EstaVacio => _tope == 0;
+
+        //Métodos
+        //Método Llenar
+        public void Llenar(int minimo, int maximo)
+        {
+            Random oRandom = new Random();
+
+            for (int i = 0; i < N; i++)
+            {
+                _arreglo[i] = oRandom.Next(minimo, maximo);
+
+            }
+            _tope = N;
+
+        }
+
+        //Método ToString
+        public override string ToString()
+        {
+            if (EstaVacio)
+            {
+                return "Esta vacío";
+            }
+
+            string cadena = string.Empty;
+            int contador = 0;
+            for (int i = 0; i < _tope; i++)
+            {
+                cadena += $"{_arreglo[i]}\t";
+                contador++;
+                if(contador > 9)
+                {
+                    contador = 0;
+                    cadena += "\n";
+                }
+            }
+            return cadena;
+        }
+    }
+}
---
+++ Arreglos.UI.Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using Arreglos.Logica;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Arreglo");
+MiArreglo oMiArreglo = new MiArreglo(100);
+oMiArreglo.Llenar(1, 20);
+
+Console.WriteLine(oMiArreglo.ToString());
+
+
+Console.ReadKey();
